2f2dbaf9d37ee4a06c710681b210dbdb12fe0e76
tnfaid <nurfaidahtitik@gmail.com>
2018-04-24 11:33:57 +0700

    slider image done

--- www/build/6.js
+++ www/build/6.js
@@ -63,20 +63,22 @@ var __metadata = (this && this.__metadata) || function (k, v) {
  * Ionic pages and navigation.
  */
 var HomePage = /** @class */ (function () {
-    function HomePage(navCtrl, navParams) {
+    function HomePage(navCtrl) {
         this.navCtrl = navCtrl;
-        this.navParams = navParams;
+        this.slideData = [{ image: "../assets/imgs/sni17_con.jpg" }, { image: "../assets/imgs/proper 17_con.jpg" }];
+        this.slideLength = false;
+        if (this.slideData.length > 0) {
+            this.slideLength = true;
+        }
     }
-    HomePage.prototype.ionViewDidLoad = function () {
-        console.log('ionViewDidLoad HomePage');
-    };
     HomePage = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
-            selector: 'page-home',template:/*ion-inline-start:"E:\Petro\ptr1_3_clone\src\pages\home\home.html"*/'<!--\n  Generated template for the HomePage page.\n\n  See http://ionicframework.com/docs/components/#navigation for more info on\n  Ionic pages and navigation.\n-->\n<ion-header>\n\n  <ion-navbar>\n    <ion-title>home</ion-title>\n  </ion-navbar>\n\n</ion-header>\n\n\n<ion-content padding>\n\n</ion-content>\n'/*ion-inline-end:"E:\Petro\ptr1_3_clone\src\pages\home\home.html"*/,
+            selector: 'page-home',template:/*ion-inline-start:"E:\Petro\ptr1_3_clone\src\pages\home\home.html"*/'<ion-header >\n  <ion-navbar no-border-bottom >\n		<button ion-button menuToggle>\n			<ion-icon name="menu"></ion-icon>\n		</button>\n\n		<ion-segment [(ngModel)]="segment" (ionChange)="updateSchedule()">\n	      <ion-segment-button value="semua">\n	        Semua\n	      </ion-segment-button>\n	      <ion-segment-button value="populer">\n	        Populer\n	      </ion-segment-button>\n	      <ion-segment-button value="terbaru">\n	        Terbaru\n	      </ion-segment-button>\n	    </ion-segment>\n\n	    <ion-buttons end>\n	      <button ion-button icon-only (click)="presentFilter()">\n	        <ion-icon ios="ios-more" md="md-more"></ion-icon>\n	      </button>\n	    </ion-buttons>\n  </ion-navbar>\n\n  <ion-toolbar no-border-top>\n    <ion-searchbar color="primary"\n                   [(ngModel)]="queryText"\n                   (ionInput)="updateSchedule()"\n                   placeholder="Search">\n    </ion-searchbar>\n  </ion-toolbar>\n</ion-header>\n\n\n<ion-content padding="true" class="has-header" >\n    <ion-slides autoplay="5000" loop="true" speed="3000" style="width:100%;height:200px;">\n	    <ion-slide *ngFor="let slide of slideData">\n	      <img src="{{slide.image}}" />\n	    </ion-slide>\n	</ion-slides>\n    <!-- ============================= LIST BERITA ========================================================= -->\n    <ion-list id="petroMobile-list4">\n      <ion-item  class="item-thumbnail-left item-text-wrap" id="petroMobile-list-item13" ui-sref="menu.judulBerita()">\n        <h4>PG Kucurkan Bantuan Untuk Korban Banjir Di Lamongan</h4>\n      </ion-item>\n      <ion-item class="item-thumbnail-left" id="petroMobile-list-item14" ui-sref="menu.judulBerita()">\n        <h2>Item</h2>\n      </ion-item>\n      <ion-item class="item-thumbnail-left" id="petroMobile-list-item15" ui-sref="menu.judulBerita()">\n        <h2>Item</h2>\n      </ion-item>\n    </ion-list>\n</ion-content>\n'/*ion-inline-end:"E:\Petro\ptr1_3_clone\src\pages\home\home.html"*/,
         }),
-        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavController */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["g" /* NavParams */]])
+        __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavController */]) === "function" && _a || Object])
     ], HomePage);
     return HomePage;
+    var _a;
 }());
 
 //# sourceMappingURL=home.js.map
