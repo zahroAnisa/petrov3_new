94e27ef930537b14de4b49bc6578554c28349481
tnfaid <nurfaidahtitik@gmail.com>
2018-04-24 09:43:36 +0700

    add side menu

---
+++ src/pages/berita/berita.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { BeritaPage } from './berita';
+
+@NgModule({
+  declarations: [
+    BeritaPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(BeritaPage),
+  ],
+})
+export class BeritaPageModule {}
