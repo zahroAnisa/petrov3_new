65a4eeafbf288867737bbd61da79a957b576d197
TinjaBoegil <bram692.didu@gmail.com>
2018-06-07 11:28:13 +0700

    solved

--- src/pages/kontak/kontak.ts
+++ src/pages/kontak/kontak.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { FormBuilder, FormGroup } from '@angular/forms';
 
 /**
  * Generated class for the KontakPage page.
--- src/pages/pedoman-wbs/pedoman-wbs.ts
+++ src/pages/pedoman-wbs/pedoman-wbs.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
  * Generated class for the PedomanWbsPage page.
